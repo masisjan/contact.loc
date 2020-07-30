a9fde2804190f1df19c39c4c1cdf160577995481
masisjan <mailjan@list.ru>
2020-07-30 20:44:12 +0400

    baza

---
+++ database/migrations/2020_07_30_201230_create_contacts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->string('phone', 100);
+            $table->string('email', 100);
+            $table->string('address', 100);
+            $table->integer('company_id')->unsigned();
+            $table->timestamps(0);
+            $table->foreign('company_id')->references('id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
